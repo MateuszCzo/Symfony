01ec18c58f2648ca2a2700e097d3ac11e3634d7e
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-03-02 23:37:21 +0100

    using variables in template

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -12,8 +12,16 @@ class MoviesController extends AbstractController
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
+        $movies = ['asd', 'sdf', 'dfg'];
         return $this->render('index.html.twig', [
-            'title' => 'movie title',
+            'title' => 'asd',
+            'movies' => $movies,
         ]);
+    }
+
+    #[Route('/movies2', name: 'app_movies2')]
+    public function index2(): Response
+    {
+        return $this->render('index2.html.twig');
     }
 }
