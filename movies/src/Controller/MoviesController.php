c53b39759a706c608c85f7c20314003710fa2230
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-03-03 15:09:44 +0100

    repository use in controller

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -2,26 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ['asd', 'sdf', 'dfg'];
-        return $this->render('index.html.twig', [
-            'title' => 'asd',
-            'movies' => $movies,
-        ]);
-    }
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findAll();
+        
+        dd($movies);
 
-    #[Route('/movies2', name: 'app_movies2')]
-    public function index2(): Response
-    {
-        return $this->render('index2.html.twig');
+        return $this->render('index.html.twig');
     }
 }
