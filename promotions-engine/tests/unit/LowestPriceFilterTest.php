6624b2e8294719bba70a5a5e1189b0bac1bab1ac
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-03-08 12:00:37 +0100

    working application

--- promotions-engine/tests/unit/LowestPriceFilterTest.php
+++ promotions-engine/tests/unit/LowestPriceFilterTest.php
@@ -19,6 +19,8 @@ class LowestPriceFilterTest extends ServiceTestCase
         $enquiry = new LowestPriceEnquiry();
         $enquiry->setProduct($product);
         $enquiry->setQuantity(5);
+        $enquiry->setRequestDate('2022-11-27');
+        $enquiry->setVoucherCode('OU812');
         $lowestPriceFilter = $this->container->get(LowestPriceFilter::class);
         $promotions = $this->promotionsDataProvider();
 
