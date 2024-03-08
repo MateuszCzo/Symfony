19730927c1e5e2e658302c29cdd2dfc80fe9cbc0
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-03-08 13:05:55 +0100

    caching

---
+++ promotions-engine/src/Cache/PromotionCache.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Cache;
+
+use App\Entity\Product;
+use App\Repository\PromotionRepository;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class PromotionCache
+{
+    private CacheInterface $cache;
+    private PromotionRepository $promotionRepository;
+
+    public function __construct(CacheInterface $cache,
+                                PromotionRepository $promotionRepository) {
+        $this->cache = $cache;
+        $this->promotionRepository = $promotionRepository;
+    }
+
+    public function findValidForProduct(Product $product, string $requestDate): ?array {
+        $key = sprintf('valid-for-product-%d', $product->getId());
+        return $this->cache->get($key, function(ItemInterface $item) use ($product, $requestDate) {
+            return $this->promotionRepository->findValidForProduct(
+                $product,
+                date_create_immutable($requestDate)
+            );
+        });
+    }
+}
--- promotions-engine/src/Controller/ProductController.php
+++ promotions-engine/src/Controller/ProductController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Cache\PromotionCache;
 use App\DTO\LowestPriceEnquiry;
-use App\Filter\PromotionFilterInterface;
+use App\Filter\PriceFilterInterface;
 use App\Repository\ProductRepository;
-use App\Repository\PromotionRepository;
 use App\Service\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,19 +17,19 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ProductController extends AbstractController
 {
     private SerializerInterface $serializer;
-    private PromotionFilterInterface $promotionFilter;
+    private PriceFilterInterface $promotionFilter;
     private ProductRepository $productRepository;
-    private PromotionRepository $promotionRepository;
+    private PromotionCache $promotionCache;
 
     public function __construct(DTOSerializer $serializer,
-                                PromotionFilterInterface $filter,
+                                PriceFilterInterface $filter,
                                 ProductRepository $productRepository,
-                                PromotionRepository $promotionRepository)
+                                PromotionCache $promotionCache)
     {
         $this->serializer = $serializer;
         $this->promotionFilter = $filter;
         $this->productRepository = $productRepository;
-        $this->promotionRepository = $promotionRepository;
+        $this->promotionCache = $promotionCache;
     }
 
     #[Route('/product/{productId}/lowest-price', name: 'lowest-price', methods: ['POST'])]
@@ -47,10 +47,7 @@ class ProductController extends AbstractController
             'json');
         $product = $this->productRepository->find($productId);
         $lowestPriceEnquiry->setProduct($product);
-        $promotions = $this->promotionRepository->findValidForProduct(
-            $product,
-            date_create_immutable($lowestPriceEnquiry->getRequestDate())
-        );
+        $promotions = $this->promotionCache->findValidForProduct($product, $lowestPriceEnquiry->getRequestDate());
         $modifiedEnquiry = $this->promotionFilter->apply($lowestPriceEnquiry, ...$promotions);
         $responseContent = $this->serializer->serialize($modifiedEnquiry, 'json');
         return new Response($responseContent, 200, ['Content-Type' => 'application/json']);
