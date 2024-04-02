4386f30ddf4a5ed23f21ec431535ff0d49738870
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-04-02 22:31:22 +0200

    navigation & security

---
+++ my_shop/src/Controller/Admin/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/admin/main', 'app_admin_main', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig');
+    }
+}
